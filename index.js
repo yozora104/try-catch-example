0e518941360a00108a8fe0919897fb7b1c0c7104
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-01-27 19:38:24 +0100

    Commit inicial.

---
+++ index.js
@@ -0,0 +1,9 @@
+function validar(nombre, email, password, repassword) {
+    // TODO: código de validación
+}
+
+try {
+    validar("John Doe", "john@doe.com", "123456", "123456")
+} catch (err) {
+    console.log(err);
+}
