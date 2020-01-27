81f41ca9a31f1d31863ce850678e2770a3b05f8c
afd <afd@alup5.academiapostal.es.local>
2020-01-27 20:42:19 +0100

    sds

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 function validar(nombre, email, password, repassword) {
     // TODO: código de validación
+    if (nombre.length==0){
+        throw new Error("El campo nombre está vacio. ");
+    }
+
+    if(email.length==0){
+            throw new Error ("El campo email está vacío. ");
+    }
+    if (password.length==0){
+        throw new Error("El campo contraseña está vacío");
+    }
+    if (repassword.length==0){
+        throw new Error("El campo repetir contraseña está vacío");
+    }
+    if (password.length !=repassword.length){
+        throw new Error("Las contraseñas son diferentes");
+    }
+        
+    return true;
 }
 
 try {
-    validar("John Doe", "john@doe.com", "123456", "123456")
+    validar("fdfdfdf","hghghg", "12345", "dfdfdf")
 } catch (err) {
-    console.log(err);
+    console.log(err.message);
 }
